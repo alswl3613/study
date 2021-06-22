10fc4446457f54b7397d243fde26fe0cdeaa99f7
박민지 <alswl3613@naver.com>
2021-06-22 17:58:57 +0900

    commit

---
+++ 210622 강원천문대/script/script.js
@@ -0,0 +1,11 @@
+$(".nav > ul > li").mouseover(function(){
+    $(this).stop().slideDown(200);
+});
+$(".nav > ul > li").mouseout(function(){
+    $(this).stop().slideUp(200);
+});
+
+$("#banner > ul > li")
+imgPrevent
+
+
